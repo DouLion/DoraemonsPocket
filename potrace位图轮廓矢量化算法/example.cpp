050e9c4d26d4dddfadecf3bc00187fcc475f5601
douliyang <734546831@qq.com>
2022-10-20 10:58:26 +0800

    添加 potrace_bitmap_s 的结构说明

--- potrace位图轮廓矢量化算法/example.cpp
+++ potrace位图轮廓矢量化算法/example.cpp
@@ -11,6 +11,32 @@ int main()
 	std::cout << std::filesystem::absolute(pngPath).string() << std::endl;
 	cv::Mat pngMat = cv::imread(pngPath.string());
 	potrace_bitmap_s* bitmap = bm_new(pngMat.cols, pngMat.rows);
+	// potrace_bitmap_s 格式简要说明
+	/*
+	    0 1 1 1 1 0 1 0  |  0 1 1
+		0 0 1 1 0 0 1 1  |  1 1 0
+		0 0 1 1 0 0 1 1	 |  1 1 0 
+		0 0 1 1 0 0 1 1	 |  1 1 0
+		0 0 1 1 0 0 1 1  |  1 0 0
+第一行	0 0 1 1 0 0 1 1  |  0 0 0
+	   | -----第一列---  | --- 第二列 ---- | ....
+
+	   宽 11 高  6
+
+	从 下到上  从 左到右 
+	potrace_word *map 
+
+	map[0]  =  0x33   第一行第一列  二进制值对应的数
+	map[1]  = 0x00	  第一行第二列  二进制值对应的数  空出来的部分认为是0 
+	map[2] = 0x33     第二行第一列
+	map[3] = 0x80     第二行第二列
+	.
+	.
+	.
+	以此类推,二值化图像的黑白都用1位表示
+	*/
+
+
 	bm_clear(bitmap, 0);
 	for (int r = pngMat.rows; r > 0; --r)
 	{
@@ -19,18 +45,16 @@ int main()
 			unsigned int byd = 0;
 			for (int kk = 0; kk < 8; ++kk)
 			{
-				byd <<= 1;
+				byd <<= 1;	// 左移一位, 最后一位置零
 				if ((c * 8 + kk) < pngMat.cols)
 				{
 					if (pngMat.ptr<unsigned int>(r - 1, c * 8 + kk)[0] > 0)
 					{
-						byd |= 1;
+						byd |= 1; // 最后一位置 1
 					}
 				}
 			}
-			byd ^= 255;
-			std::cout << byd << " ";
-
+			byd ^= 255; // 不知道为啥, 应该跟后面算法的实现有关系, 面内用1表示 面外用0表示
 			*bm_index(bitmap, c * 8, pngMat.rows - r) |= ((potrace_word)byd) << (8 * (BM_WORDSIZE - 1 - (c % BM_WORDSIZE)));;
 		}
 		std::cout << std::endl;
