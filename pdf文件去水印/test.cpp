fba6e005a652b39108af389739ec009183e8e952
douliyang <734546831@qq.com>
2022-10-28 13:17:03 +0800

    放弃podofo,中文支持上似乎有问题,准备使用libmupdf

--- pdf文件去水印/test.cpp
+++ pdf文件去水印/test.cpp
@@ -1,53 +1,23 @@
-#pragma once
-//#define PODOFO_HAVE_BOOST
-#include <podofo/podofo.h>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
 
-#include <iostream>
-#include <stack>
-#include <algorithm>
-#include <string>
-#include <iomanip>
-#include <cstdio>
-#include "PdfContentsGraph.h"
-using namespace std;
-using namespace PoDoFo;
-
-int main(int argc, char* argv[])
+int main()
 {
-	bool all_pages = false;
-	int firstPageNo = 0;
-	string inputFileName;
-	inputFileName = "../../../test.pdf";
 
-	try
-	{
-		PdfMemDocument doc(inputFileName.c_str());
-		if (!doc.GetPageCount())
-		{
-			std::cerr << "This document contains no page!" << std::endl;
-			return 1;
-		}
-
-		int toPage = all_pages ? doc.GetPageCount() : firstPageNo + 1;
-		for (int i = firstPageNo; i < toPage; ++i)
-		{
-			cout << "Processing page " << setw(6) << (i + 1) << "..." << std::flush;
-			PdfPage* page = doc.GetPage(i);
-			PODOFO_RAISE_LOGIC_IF(!page, "Got null page pointer within valid page range");
-
-			PdfContentsTokenizer tokenizer(page);
-			PdfContentsGraph grapher(tokenizer);
-			grapher.WriteToStdErr();
-
-			cout << " - page ok" << endl;
-		}
-	}
-	catch (const PdfError& e)
-	{
-		e.PrintErrorMsg();
-		return e.GetError();
-	}
-
-	cout << endl;
+	cv::Mat lutRND = cv::Mat::zeros(256, 1, CV_8UC3);
+	// B G R  , 这种渲染方式只支持 单通道 或 三通道
+	lutRND.at<cv::Vec3b>(13, 0) = cv::Vec3b(0xaa, 0x5c, 0x2a);
+	lutRND.at<cv::Vec3b>(14, 0) = cv::Vec3b(0x00, 0xe0, 0xff);
+	lutRND.at<cv::Vec3b>(15, 0) = cv::Vec3b(0x21, 0x6c, 0xf3);
+	lutRND.at<cv::Vec3b>(16, 0) = cv::Vec3b(0x36, 0x41, 0xef);
+	cv::Mat src = cv::imread("src.png", -1);
+	cv::Mat dst;
+	cv::applyColorMap(src, dst, lutRND);
+	cv::imshow("Render Image", dst);
+	cv::namedWindow("Render Image");
+	cv::waitKey(0);
+	cv::imwrite("convert.png", dst);
 	return 0;
+
 }
