1f446e00676aa7174eb9562034d4aac2ab3cb15f
douliyang <734546831@qq.com>
2022-10-31 11:21:44 +0800

    多线程支持

--- mbtile_merge/main.cpp
+++ mbtile_merge/main.cpp
@@ -14,6 +14,17 @@
 #include <filesystem>
 #include <json/json.h>
 #include <fstream>
+#include <thread>
+#include <chrono>
+#define DETECT_LEAK 0
+#if DETECT_LEAK		// 检查没有内存泄漏
+#include <vld.h>
+#endif
+
+
+#if SINGLE_THREAD_MODE
+
+#endif
 namespace tzxutils	// 防止有些函数名 或者 .. 重复
 {
 	// 逗号 字符
@@ -24,7 +35,7 @@ namespace tzxutils	// 防止有些函数名 或者 .. 重复
 	// 查询语句
 	const char* SQL_QUERY_TILES = "select zoom_level, tile_column, tile_row, tile_data  from tiles;";
 	const char* SQL_QUERY_METADATA = "select name, value from metadata;";
-
+	// 插入语句
 	const char* SQL_INSERT_TILES = "insert into tiles (zoom_level, tile_column, tile_row, tile_data) values (?,?,?,?);";
 	const char* SQL_INSERT_METADAT = "insert into metadata (name, value) values (?,?);";
 
@@ -107,12 +118,14 @@ namespace tzxutils	// 防止有些函数名 或者 .. 重复
 			output_name = {};
 			tile_data = {};
 			meta_data = {};
+			_thread_count = 0;
+			max_thread = 16;
 		}
 		std::string output_name;
 		std::vector<std::string> input_names;
 		std::map<std::string, mtv_tile_string::LCRData>	tile_data;
 		std::map<std::string, MetaInfo> meta_data;
-
+		int max_thread;
 
 		mtv_tile_string::LCRData read_mbtiles(const std::string& mbtile_path, MetaInfo& info)
 		{
@@ -181,24 +194,21 @@ namespace tzxutils	// 防止有些函数名 或者 .. 重复
 				int levle = sqlite3_column_int(tile_stat, 0);
 				int col = sqlite3_column_int(tile_stat, 1);
 				int row = sqlite3_column_int(tile_stat, 2);
-				_max_level = std::max(levle, _max_level);
+				_max_level = std::max(levle, _max_level);	//  最大最小层级
 				_min_level = std::min(levle, _min_level);
-				/*if (_max_col.find(levle) == _max_col.end())
+				if (l_c_r_record[levle][col].empty())
 				{
-					_max_col[levle] = -10e6;
-					_min_col[levle] = 10e6;
-					_max_row[levle] = -10e6;
-					_min_row[levle] = 10e6;
+					l_c_r_record[levle][col] = {};
 				}
-				_max_col[levle] = std::max(col, _max_col[levle]);
-				_min_col[levle] = std::min(col, _min_col[levle]);
-				_max_row[levle] = std::max(_max_row[levle], row);
-				_min_row[levle] = std::min(_min_row[levle], row);*/
-				l_c_r_record[levle][col] = row;
+				l_c_r_record[levle][col][row] = 1;
 				std::string info = read_blob_data(tile_stat, 3);
 				if (!info.empty())	// 一般不会有这个情况
 				{
 					retData[levle][col][row] = info;
+				}
+				else
+				{
+					std::cerr << levle << "-" << col << "-" << row << std::endl;
 				}
 				result = sqlite3_step(tile_stat);
 			}
@@ -233,42 +243,30 @@ namespace tzxutils	// 防止有些函数名 或者 .. 重复
 		bool merge_tile_data()
 		{
 			mtv_tile_string::LCRData target_medata_data;
-		
+
 			// 考虑 使用多线程 提高效率
-		
 			for (auto [l, c_r] : l_c_r_record)
 			{
-				for (auto [c, r] : c_r)
+				for (auto [c, rs] : c_r)
 				{
-					mvt_tile _merge_mvt_tile;
-					for (auto [_name, _lcrdata] : tile_data)
+					for (auto [r, oc]: rs)
 					{
-						std::string data_str = _lcrdata[l][c][r]; // 检查不存在的是否补齐为空字符串
-						if (data_str.empty())
+						std::thread _merge_thread(&TaskInfo::merge_tile_thread, this, l, c, r, std::ref(target_medata_data));
+						_thread_count++;
+						while (_thread_count > max_thread)
 						{
-							continue;
+							std::this_thread::sleep_for(std::chrono::milliseconds(2));
 						}
-
-						mvt_tile _tmp_tile;
-						bool isCps = true;
-						if (_tmp_tile.decode(data_str, isCps)) {
-							for (auto _tlayer : _tmp_tile.layers)
-							{
-								_merge_mvt_tile.layers.emplace_back(_tlayer);
-							}
-						}
+						_merge_thread.detach();
 					}
-					if (_merge_mvt_tile.layers.size() > 0)
-					{
-						target_medata_data[l][c][r] = _merge_mvt_tile.encode();
-					}
+					
 				}
-				/*if (l >= 4)
-				{
-					break;
-				}*/
 			}
 
+			while (_thread_count > 0)
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			}
 			MetaInfo merge_meta_info = merge_meta_data();
 			return insert_tile_data(target_medata_data, merge_meta_info);
 		}
@@ -327,7 +325,7 @@ namespace tzxutils	// 防止有些函数名 或者 .. 重复
 			float centerx = (_max_lgtd + _min_lgtd) / 2;
 			float centery = (_max_lttd + _min_lttd) / 2;
 			merge_meta_info.center = std::to_string(centerx) + "," + std::to_string(centery);
-			merge_meta_info.bounds = std::to_string(_min_lgtd) + "," + std::to_string(_min_lttd) + "" + std::to_string(_max_lgtd) + "," + std::to_string(_max_lttd);
+			merge_meta_info.bounds = std::to_string(_min_lgtd) + "," + std::to_string(_min_lttd) + "," + std::to_string(_max_lgtd) + "," + std::to_string(_max_lttd);
 			merge_meta_info.type = "overlay";
 			merge_meta_info.format = "pbf";
 			merge_meta_info.generator = "tzx tile merge";
@@ -337,6 +335,44 @@ namespace tzxutils	// 防止有些函数名 或者 .. 重复
 			return merge_meta_info;
 		}
 	private:
+
+		/// <summary>
+		/// 合并tile的任务线程
+		/// </summary>
+		/// <param name="level"></param>
+		/// <param name="col"></param>
+		/// <param name="row"></param>
+		/// <param name="write_data"></param>
+		void merge_tile_thread(const int& level, const int& col, const int& row, mtv_tile_string::LCRData& write_data)
+		{
+			mvt_tile _merge_mvt_tile;
+			for (auto [_name, _lcrdata] : tile_data)
+			{
+				std::string data_str = _lcrdata[level][col][row]; // 检查不存在的是否补齐为空字符串
+				if (data_str.empty())
+				{
+					continue;
+				}
+
+				mvt_tile _tmp_tile;
+				bool isCps = true;
+				if (_tmp_tile.decode(data_str, isCps)) {
+					for (auto _tlayer : _tmp_tile.layers)
+					{
+						_merge_mvt_tile.layers.emplace_back(_tlayer);
+					}
+				}
+			}
+			if (_merge_mvt_tile.layers.size() > 0)
+			{
+				write_data[level][col][row] = _merge_mvt_tile.encode();
+			}
+			else
+			{
+				std::cerr << level << "-" << col << "-" << row << " 空" << std::endl;;
+			}
+			_thread_count--;
+		}
 
 		/// <summary>
 		/// 将压缩后的数据写回
@@ -366,7 +402,7 @@ namespace tzxutils	// 防止有些函数名 或者 .. 重复
 			if (SQLITE_OK != sqlite3_exec(pDB, SQL_CREATE_TILES, NULL, NULL, &errMsg))
 			{
 
-				std::cout << "创建 metadata 表失败: " << sqlite3_errmsg(pDB) << std::endl;
+				std::cout << "创建 tiles 表失败: " << sqlite3_errmsg(pDB) << std::endl;
 				sqlite3_free(errMsg);
 				sqlite3_close(pDB);
 				return false;
@@ -401,9 +437,7 @@ namespace tzxutils	// 防止有些函数名 或者 .. 重复
 
 					sqlite3_reset(inset_meta_stmt);
 				}
-				//sqlite3_exec(pDB, "COMMIT TRANSACTION", NULL, NULL, &errMsg);
 				sqlite3_finalize(inset_meta_stmt);
-				// name, description, version, minzoom, maxzoom, center, bounds, type, format, generator, generator_options, json
 			}
 			else {
 				fprintf(stderr, "SQL error: %s\n", errMsg);
@@ -434,7 +468,6 @@ namespace tzxutils	// 防止有些函数名 或者 .. 重复
 						}
 					}
 				}
-				//sqlite3_exec(pDB, "COMMIT TRANSACTION", NULL, NULL, &errMsg);
 				sqlite3_finalize(inset_tile_stmt);
 			}
 			else {
@@ -449,32 +482,37 @@ namespace tzxutils	// 防止有些函数名 或者 .. 重复
 		// 多个mbtiles的范围
 		int _max_level = -10e6;
 		int _min_level = 10e6;
-		std::map<int, int> _max_row;
-		std::map<int, int> _min_row;
-		std::map<int, int> _max_col;
-		std::map<int, int> _min_col;
-		std::map<int, std::map<int, int>> l_c_r_record;
+		std::map<int, std::map<int, std::map<int, int>>> l_c_r_record;
 		// 最大经纬范围包围框
 		float _max_lgtd = -10e6;
 		float _min_lgtd = 10e6;
 		float _max_lttd = -10e6;
 		float _min_lttd = 10e6;
+		std::atomic<unsigned int> _thread_count;
 	};
 
 	/// <summary>
 	/// 解析参数文件
 	/// </summary>
 	/// <returns></returns>
-	std::map<std::string, std::vector<std::string>> phrase_param_file_task()
+	std::map<std::string, std::vector<std::string>> phrase_param_file_task(int& max_thread_limit)
 	{
 		std::map<std::string, std::vector<std::string>> retData;
 		Json::Reader reader;
 		Json::Value root;
 		std::fstream input;
-		std::cout << std::filesystem::absolute(std::filesystem::path("../../../param.json").string()) << std::endl;;
-		input.open("../../../param.json", std::ios::binary | std::ios::in);
+		std::cout << std::filesystem::absolute(std::filesystem::path("param.json").string()) << std::endl;;
+		input.open("param.json", std::ios::binary | std::ios::in);
 		if (reader.parse(input, root))
 		{
+			if (root["thread_limit"].isNull())
+			{
+				max_thread_limit = 16;
+			}
+			else
+			{
+				max_thread_limit = root["thread_limit"].asInt();
+			}
 			for (int i = 0; i < root["tasks"].size(); ++i)
 			{
 				std::string _output = root["tasks"][i]["output"].asString();
@@ -494,10 +532,12 @@ namespace tzxutils	// 防止有些函数名 或者 .. 重复
 using namespace tzxutils;
 int main()
 {
-	std::map<std::string, std::vector<std::string>> tasks_params = phrase_param_file_task();
+	int thread_limit = 0;
+	std::map<std::string, std::vector<std::string>> tasks_params = phrase_param_file_task(thread_limit);
 	for (auto [_output_name, _input_names] : tasks_params)
 	{
 		TaskInfo _task_info;
+		_task_info.max_thread = thread_limit;
 		_task_info.input_names = _input_names;
 		_task_info.output_name = _output_name;
 		_task_info.read_data();
